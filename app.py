912ef08f213f79a3b8f5c693f9573d5dff3d6e4d
cop1fab <bienaimecopain@gmail.com>
2020-03-24 12:12:30 +0200

    :sparkles: added create and get drivers

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify, request, make_response
 from database.db import initialize_db
+from database.models import Driver
 
 
 app = Flask(__name__)
@@ -13,6 +14,19 @@ initialize_db(app)
 @app.route("/", methods=['GET'])
 def home():
     return "Welcome to motoly, your #1 app to manage electric vehicle business in Rwanda"
+
+
+@app.route("/driver", methods=["POST"])
+def create_driver():
+    driver = request.get_json()
+    Driver(name=driver["name"]).save()
+    return make_response(jsonify(driver)), 201
+
+
+@app.route("/drivers", methods=["GET"])
+def get_drivers():
+    drivers = Driver.objects().to_json()
+    return make_response(drivers), 200
 
 
 if __name__ == '__main__':
