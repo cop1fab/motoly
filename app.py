24b70d5c2d490c3123a23fc99619d415364621a4
cop1fab <bienaimecopain@gmail.com>
2020-03-24 11:59:04 +0200

    :card_file_box: added MongoDB

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from database.db import initialize_db
+
 
 app = Flask(__name__)
 
+app.config['MONGODB_SETTINGS'] = {
+    'host': 'mongodb://localhost/motors'
+}
+initialize_db(app)
 
-@app.route("/", methods=['GET', 'POST', 'PUT'])
+
+@app.route("/", methods=['GET'])
 def home():
-    return "Yaaay! Here we are, now let's start building!!"
+    return "Welcome to motoly, your #1 app to manage electric vehicle business in Rwanda"
 
 
 if __name__ == '__main__':
